024e58b330625407b8eca790ab8be8afa840196a
Paul T. Bauman <ptbauman@gmail.com>
2016-12-12 15:47:50 -0500

    More debugging GRINS setup.

--- scripts/grins/configure.sh
+++ scripts/grins/configure.sh
@@ -2,7 +2,17 @@
 #REQUIRED: APPLICATION_REPO
 init_script || exit 1
 
-print_and_run $REPO_DIR/configure METHOD=$METHOD
+printf "\nWanting to build in $BUILD_ROOT/$FEMPUTER_BUILD_DIRNAME\n"
+
+cd $BUILD_ROOT/$FEMPUTER_BUILD_DIRNAME
+exitIfReturnCode $?
+
+printf "\nCurrent directory is $PWD\n"
+
+print_and_run module load gcc mpich openblas petsc boost hdf5 vtk gsl libmesh cantera antioch
+exitIfReturnCode $?
+
+print_and_run $REPO_DIR/configure 
 exitIfReturnCode $?
 
 printf "\nSuccessfully configured GRINS.\n"
--- scripts/grins/make_check.sh
+++ scripts/grins/make_check.sh
@@ -2,6 +2,13 @@
 #REQUIRED: APPLICATION_REPO
 init_script || exit 1
 
+SUBDIR=$BUILD_ROOT/$FEMPUTER_BUILD_DIRNAME
+cd "$SUBDIR"
+exitIfReturnCode $?
+
+print_and_run module load gcc mpich openblas petsc boost hdf5 vtk gsl libmesh cantera antioch
+exitIfReturnCode $?
+
 print_and_run make -j 32 check 
 exitIfReturnCode $?
 
