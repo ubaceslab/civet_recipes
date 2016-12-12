9d778f76cf7062a9b423e9fc44214cf7bd025c21
Paul T. Bauman <ptbauman@gmail.com>
2016-12-12 12:55:59 -0500

    It's just bootstrap, not bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,9 +3,10 @@
 init_script || exit 1
 
 cd "$REPO_DIR"
+printf "\nCurrent directory is $PWD\n"
 exitIfReturnCode $?
 
-print_and_run ./bootstrap.sh 
+print_and_run ./bootstrap 
 exitIfReturnCode $?
 
 printf "\nSuccessfully bootstrapped $REPO_DIR\n"
