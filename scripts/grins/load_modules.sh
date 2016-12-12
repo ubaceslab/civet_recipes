38be9323445f0fe943e6212a94c051399f1f504d
Paul T. Bauman <ptbauman@gmail.com>
2016-12-12 12:56:44 -0500

    More relevant echo text

--- scripts/grins/load_modules.sh
+++ scripts/grins/load_modules.sh
@@ -7,6 +7,6 @@ init_script || exit 1
 print_and_run module load gcc mpich openblas petsc boost hdf5 vtk gsl libmesh cantera antioch 
 exitIfReturnCode $?
 
-printf "\nSuccessfully ran $RUN_CMD\n"
+printf "\nSuccessfully loaded GRINS modules\n"
 
 exit 0
