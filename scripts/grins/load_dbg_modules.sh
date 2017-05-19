310c9592ea2851cbbd7b725b06e51ff1a85cac90
Paul T. Bauman <ptbauman@gmail.com>
2017-05-19 08:32:28 -0400

    Update to use libmesh-grins-min module
    This is the libmesh build at the minimum hash needed for GRINS.

--- scripts/grins/load_dbg_modules.sh
+++ scripts/grins/load_dbg_modules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 module load gcc mpich openblas petsc boost-dbg hdf5 vtk-dbg cppunit-dbg;
-module load gsl libmesh-dbg antioch-dbg 
+module load gsl libmesh-grins-min-dbg antioch-dbg 
 
 printf "\nSuccessfully loaded GRINS modules\n"
--- scripts/grins/load_modules.sh
+++ scripts/grins/load_modules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 module load gcc mpich openblas petsc boost hdf5 vtk cppunit;
-module load gsl libmesh cantera antioch 
+module load gsl libmesh-grins-min cantera antioch 
 
 printf "\nSuccessfully loaded GRINS modules\n"
