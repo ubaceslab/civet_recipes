0a14679fb558b6fa0c86ed7f5a1d48f0bceae23e
Paul T. Bauman <ptbauman@gmail.com>
2018-07-13 11:56:12 -0400

    Need to explicitly enable hdf5 now

--- scripts/libmesh/configure.sh
+++ scripts/libmesh/configure.sh
@@ -15,7 +15,7 @@ print_and_run module list
 exitIfReturnCode $?
 
 FEMPUTER_LIBMESH_INSTALL_DIR="$BUILD_ROOT/$FEMPUTER_INSTALL_DIRNAME"
-FEMPUTER_LIBMESH_COMMON_CONFIG_OPTIONS="--enable-everything --with-metis=PETSc --with-cppunit-prefix=${CPPUNIT_DIR:?undefined} --with-vtk-lib=${VTK_LIB:?undefined} --with-vtk-include=${VTK_INCLUDE:?undefined} --with-methods=${METHODS:?undefined} --prefix=${FEMPUTER_LIBMESH_INSTALL_DIR:?undefined}"
+FEMPUTER_LIBMESH_COMMON_CONFIG_OPTIONS="--enable-everything --with-metis=PETSc --with-cppunit-prefix=${CPPUNIT_DIR:?undefined} --with-vtk-lib=${VTK_LIB:?undefined} --with-vtk-include=${VTK_INCLUDE:?undefined} --with-methods=${METHODS:?undefined} --prefix=${FEMPUTER_LIBMESH_INSTALL_DIR:?undefined} --enable-hdf5"
 
 print_and_run $REPO_DIR/configure $FEMPUTER_LIBMESH_COMMON_CONFIG_OPTIONS $FEMPUTER_LIBMESH_ADDITONAL_CONFIG_OPTIONS
 exitIfReturnCode $?
